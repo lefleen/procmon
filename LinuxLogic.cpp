a42f4706ec8509f150a26f3496752a9b47fee823
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-03-06 15:48:13 +0300

    bug fixes

--- LinuxLogic.cpp
+++ LinuxLogic.cpp
@@ -13,12 +13,8 @@ Result ProcmonLogic::NameProc::get(const ProcessDescriptorRAII& descriptor_proce
     std::string path_to_process = "/proc/" + std::to_string(descriptor_process.get()) +  "/stat";
     int input_file_descriptor = 0;
 
-    std::cout << descriptor_process.get();
-
     if((input_file_descriptor = open(path_to_process.c_str(), O_RDONLY))) return Result::failure;
     if(read(input_file_descriptor, &buffer, BUFFER_SIZE)) return Result::failure;
-
-    std::cout << descriptor_process.get() << "\t" << buffer << std::endl;
 
     return Result::successful;
 }
@@ -59,7 +55,7 @@ Result ProcmonLogic::Manage::get_parameters_processes(parameters_process& params
 
 Result ProcmonLogic::AllData::get_all_data_process(ProcessDescriptorRAII& descriptor_process, const parameters_process& params, Process& process)
 {
-    ProcmonLogic::DescriptorProc::get(descriptor_process, pid);
+    ProcmonLogic::DescriptorProc::get(descriptor_process, process.pid);
     if(ProcmonLogic::NameProc::get(descriptor_process) == Result::failure) process.name = L"NoName\0";
 
     return Result::successful;
